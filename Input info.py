f7015438e80964ce08d82f3aefdf4a8c51bb621c
mroy19 <mroy19@students.kennesaw.edu>
2024-11-01 17:07:34 -0400

    Commit

---
+++ Input info.py
@@ -0,0 +1,28 @@
+
+#for the doctor to input the information 
+Label=''
+doctor=('Doctor name:')
+patient=input('Patient Name: ')
+dob=input('Patient DOB: ')
+pharmacy =input ('Patient Pharmacy Address: ')
+pharmacy_num=input('Pharmacy Phone Number: ')
+pharmacy_name=input('Pharmacy Name: ')
+email=input('Patient Email: ')
+EHR=input('Facility EHR system: ')
+medication1=input('Patient medication 1: ')
+medication2=input('Patient medication 2: ')
+medication1_dosage=input ('Medication 1 Dosage: ')
+medication2_dosage= input('Medication 2 Dosage: ')
+medication1_time=input('What time does the patient take medication 1: ')
+medication1_time=input('What time does the patient take medication 2: ')
+Label=print(patient,dob,medication1,medication1_dosage,medication1_time,doctor, pharmacy_name)
+#patient must input this information with their consent
+family=input ('Family to include: ')
+family_email=input ('Family email: ')
+family_num=input('Family phone number: ')
+
+
+
+Label()
+
+
