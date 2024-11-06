e9b3c6764a2ce9c75a7800c3292f4d987d171fb9
mroy19 <mroy19@students.kennesaw.edu>
2024-11-06 14:27:19 -0500

    Update Input info.py
    Created a list of items we would need to input into the application in order for it to create a medication reminder, notifications to those included, and patient information for QR Code.

--- Input info.py
+++ Input info.py
@@ -1,5 +1,6 @@
 
 #for the doctor to input the information 
+
 Label=''
 doctor=('Doctor name:')
 patient=input('Patient Name: ')
@@ -16,7 +17,9 @@ medication2_dosage= input('Medication 2 Dosage: ')
 medication1_time=input('What time does the patient take medication 1: ')
 medication1_time=input('What time does the patient take medication 2: ')
 Label=print(patient,dob,medication1,medication1_dosage,medication1_time,doctor, pharmacy_name)
+
 #patient must input this information with their consent
+
 family=input ('Family to include: ')
 family_email=input ('Family email: ')
 family_num=input('Family phone number: ')
